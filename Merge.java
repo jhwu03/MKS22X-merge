14a296372e6615f847559194a9ce0341e932b162
jwu03 <jwu03@stuy.edu>
2019-03-17 22:55:42 -0400

    working on merge

--- Merge.java
+++ Merge.java
@@ -1,9 +1,13 @@
 public class Merge{
   public static void mergesort(int[]data){
-    int n = data.length;
-    if(n == 1){
-      return data[0];
-    }
-    mergesort(data,0,data.length-1);
+    if (data.length <= 1)
+     return;
+    mergesortH(data,0,data.length-1);
+  }
+
+  public static void mergesortH(int[] data,int lo,int hi){
+    if (lo >= hi)
+      return;
+      
   }
 }
