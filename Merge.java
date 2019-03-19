eb605855dec2f6206de437d2bce8c7072053894c
jwu03 <jwu03@stuy.edu>
2019-03-19 18:11:17 -0400

    working on merge

--- Merge.java
+++ Merge.java
@@ -13,5 +13,24 @@ public class Merge{
       merge(data,lo,middle,hi);
     }
   }
-  
+
+  public static void merge(int[] data, int lo, int middle, int hi){
+    int half1 = middle - lo + 1;
+    int half2 = hi - middle;
+    int[] R = new int[half1];
+    int[] C = new int[half2];
+
+    for(int i = 0; i < half1; i++){
+      R[i] = data[lo + i];
+    }
+    for(int i = 0;i < half2; i++){
+      C[i] = data[(middle + 1)+ i];
+    }
+
+    int i, j;
+    i = 0;
+    j = 0;
+
+  }
+
 }
