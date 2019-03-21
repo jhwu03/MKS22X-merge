ab776ab647614da34ac935cc5e520f5ac6250952
jwu03 <jwu03@stuy.edu>
2019-03-20 21:51:47 -0400

    added insertion sort to increase spped

--- Merge.java
+++ Merge.java
@@ -2,7 +2,7 @@ public class Merge{
   public static void mergesort(int[]data){
     if (data.length <= 1)
      return;
-    mergesortH(data,0,data.length-1);
+    mergesortO(data,0,data.length-1);
   }
 
   public static void mergesortH(int[] data,int lo,int hi){
